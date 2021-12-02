68fb660e2099369e9485593476fd652a4dcd7039
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2021-12-02 15:50:59 +0500

    StoreRequest set up

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class StoreProjectRequest extends FormRequest
 {
@@ -13,7 +15,8 @@ class StoreProjectRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        abort_if(Gate::denies('project_store'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        return true;
     }
 
     /**
@@ -24,7 +27,9 @@ class StoreProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => [
+                'required'
+            ],
         ];
     }
 }
